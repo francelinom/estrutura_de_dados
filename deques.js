9612435576142937cc2ffd2d74f177a021eb9c1b
Francelino <fmarcilio1989@gmail.com>
2022-06-07 18:27:26 -0300

    iniciando o deque

---
+++ deques.js
@@ -0,0 +1,17 @@
+/**
+ * Principais operações em deques:
+ * insertFront -> Insere um elemento na frente
+ * insertLast -> Insere um elemento na cauda
+ * deleteFront -> Remove o elemento que está na frente
+ * deleteLast -> Remove o elemento que está na cauda
+ * front -> Retorna o elemento que está na frente
+ * rear -> Retorna o elemento que está na cauda
+ * isEmpty -> Retorna um valor do tipo Boolean indicando se o deque está ou não vazio
+ * isFull -> retona um valor do tipo Boolean indicando se o deque está ou não cheio
+ */
+
+class Deque {
+    constructor() {
+        this.itens = [];
+    }
+}
